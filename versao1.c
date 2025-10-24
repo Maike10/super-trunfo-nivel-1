f3eef094e140093c8be14523799b41cb0b4f8ef0
Maike10 <Maike10@users.noreply.github.com>
2025-10-24 02:53:06 +0000

    super_trunfo_v1

---
+++ versao1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (){
+    int carta = 1;
+    int estado = 'a';
+    int pontosT = 30;
+    char codigo [10] = "a01";
+    char cidade [20] = "goiania";
+    double população = 1400000;
+    float area = 7500.52;
+    float pib = 257.30;
+
+    printf ("carta: %d\n" , carta);
+    printf ("estado: %c\n" , estado);
+    printf ("codigo: %s\n" , codigo);
+    printf ("cidade: %s\n" , cidade);
+    printf ("população: %.0f\n" , população);
+    printf ("área: %.2f\n" , area);
+    printf ("pib: %.2f\n" , pib);
+    printf ("numeros de pontos turisticos: %d\n" , pontosT);
+
+
+    
+
+}
