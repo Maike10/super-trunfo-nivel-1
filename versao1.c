15023fb1845a4e7040a7f71c0e1ca46cad2c0975
Maike10 <Maike10@users.noreply.github.com>
2025-10-28 22:45:38 +0000

    supertrunfo_v1.2

--- versao1.c
+++ versao1.c
@@ -2,12 +2,14 @@
 
 int main (){
    printf ("Super Trunfo!\n");
+
+   printf ("\n");
    
     int carta = 1;
     int estado = 'a';
     int pontosT = 30;
     char codigo [10] = "a01";
-    char cidade [20] = "goiania";
+    char cidade [20] = "Goiania";
     double população = 1400000;
     float area = 7500.52;
     float pib = 257.30;
@@ -22,6 +24,29 @@ int main (){
     printf ("numeros de pontos turisticos: %d\n" , pontosT);
 
 
-    
+    int carta_2 = 2;
+    int estado_2 = 'b';
+    int pontosT_2 = 36;
+    char codigo_2 [10] = "a02";
+    char cidade_2 [20] = "Salvador";
+    double população_2 = 25000000;
+    float area_2 = 8316.30;
+    float pib_2 = 290.10;
+
+    printf ("\n");
+
+    printf ("carta: %d\n" , carta_2);
+    printf ("estado: %c\n" , estado_2);
+    printf ("codigo: %s\n" , codigo_2);
+    printf ("cidade: %s\n", cidade_2);
+    printf ("população: %.0f\n" , população_2);
+    printf ("área: %.2f\n" , area_2);
+    printf ("pib: %.2f\n" , pib_2);
+    printf ("numeros de pontos turisticos: %d\n" , pontosT_2);
+
+    printf ("\n");
+
+
+    return 0;
 
 }
